9a90c8a88ac1352c104e4f3364d54ecef6357727
Дмитрий <osdal67@gmail.com>
2022-10-30 07:39:56 +0200

    Первое приближение

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let menu_item_name = document.querySelectorAll('.menu-item-name');
+for (let i = 0; i < menu_item_name.length; i++) {
+	menu_item_name[i].style.color = '#eb4022';
+	// let text_menu_item = menu_item_name[i].textContent.replace(' ', br);
+	// console.log(br);
+
+}
